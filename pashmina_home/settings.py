cd037a0de4ecb12a27eadb37ead22661738305f6
shiva <sb4_cbu@yahoo.com>
2024-01-19 19:08:06 +0000

    trigger deployment

--- pashmina_home/settings.py
+++ pashmina_home/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -72,7 +72,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # Remove below line on development and use during deployment only
-    # 'allauth.account.middleware.AccountMiddleware', 
+    'allauth.account.middleware.AccountMiddleware', 
 ]
 
 ROOT_URLCONF = 'pashmina_home.urls'
